bf92539a2a452fb5aaf39dca4ec4f01bae13bf37
Dare Adewole <dare.dev.adewole@gmail.com>
2019-08-16 13:25:26 +0100

    Changed Socket transport to WEBSOCKETS

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import axios from 'axios'
 import VueSocketIO from 'vue-socket.io'
+import IO from 'socket.io-client'
 import Zutre from 'zutre'
 import App from './App.vue'
 import router from './router'
@@ -11,13 +12,21 @@ import 'zutre/dist/zutre.css'
 Vue.use(Zutre)
 
 const CLOUD_SERVER = 'https://watersupervisorycontrol.herokuapp.com';
-const LOCAL_SERVER = 'http://10.150.0.2:5555';
-const SERVER = CLOUD_SERVER;
+const CLOUD_SOCKET_SERVER = 'ws://watersupervisorycontrol.herokuapp.com';
+// const LOCAL_SERVER = 'http://192.168.1.108:5555';
+// const LOCAL_SOCKET_SERVER = 'ws://192.168.1.108:5555';
+// const SERVER = LOCAL_SERVER;
 
 // Vue Socket Setup
+const socket = IO(`${CLOUD_SOCKET_SERVER}/wms`, {
+  transports: [ 'websocket' ],
+  timeout: 5000,
+  reconnectionAttempts: 'Infinity'
+})
 Vue.use(new VueSocketIO({
-  debug: false,
-  connection: `${SERVER}/wms?token=hEmG3Zw1frt5ZGuQ609II7KRstubkBG5`,
+  debug: true,
+  // connection: `${SERVER}/wms?token=hEmG3Zw1frt5ZGuQ609II7KRstubkBG5`,
+  connection: socket,
   vuex: {
     store,
     actionPrefix: 'WMS_SOCKET_',
@@ -27,7 +36,7 @@ Vue.use(new VueSocketIO({
 
 // Axios Setup
 var axiosInstance = axios.create({
-  baseURL: `${SERVER}/api`,
+  baseURL: `${CLOUD_SERVER}/api`,
   headers: {
     'Content-Type': 'application/json'
   }
